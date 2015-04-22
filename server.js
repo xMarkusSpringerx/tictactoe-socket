027cb8291dfdedff816ce88346d07e6718b43d44
xMarkusSpringerx <markus.springer1994@googlemail.com>
2015-04-22 23:15:34 +0200

    Added rethinkDB to App

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 
+r = require('rethinkdb')
+r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
+
+    // Reset DB on every restart of Node (only for testing)
+
+    r.db('tictactoe-socket').tableDrop('user').run(conn, function(err) {
+        console.log(err);
+    });
+
+
+    // Can't connect to Server
+    if(err) throw err;
+
+    // Create new table
+    r.db('tictactoe-socket').tableCreate('user').run(conn, function(err, res) {
+
+        //Can't connect to DB
+        if(err) throw err;
+        console.log(res);
+    });
+
+});
+
+
+
 server.listen(3000);
 
 app.use(express.static(__dirname + '/app'));
